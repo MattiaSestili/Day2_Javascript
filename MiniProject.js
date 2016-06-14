ea99ceeaef4d28a71b269f192922da522a9b3927
aldess <mattia.sestili@gmail.com>
2016-06-14 20:04:00 +0100

    Edit do while

--- MiniProject.js
+++ MiniProject.js
@@ -6,36 +6,43 @@ event1.addEventListener('click', guessNumber, false);
 
 var numberGuesses = 0;
 
-function numberGuess(){
-    
-    var numberToGuess = Math.floor(Math.random()*100);
+function numberGuess()
+{
+    var numberToGuess = Math.floor(Math.random()*10)+1;
     return numberToGuess;
 }
 
 function guessNumber()
 {
     var userinput = parseInt(document.getElementById("input1").value);
-    
     var numberToGuess = numberGuess();
-    
-    if (userinput > numberToGuess){
+    var i = 0;
+   
+    //do
+    //{
         
-        document.getElementById("demo").innerHTML = "Too high";
-        document.getElementById("demo2").innerHTML = "The correct number was " + numberToGuess;
-        countGuesses();
-    }
-    else if(userinput < numberGuess().numberToGuess)
-    {
-        document.getElementById("demo").innerHTML = "Too low";
-        document.getElementById("demo2").innerHTML = "The correct number was " + numberToGuess;
-        countGuesses();
-    }
-    else 
-    {
-        document.getElementById("demo").innerHTML = "Well Done";
-        document.getElementById("demo2").innerHTML = "The correct number was " + numberToGuess;
-        countGuesses();
-    }
+        if (userinput > numberToGuess)
+        {
+            document.getElementById("demo").innerHTML = "Too high";
+            document.getElementById("demo2").innerHTML = "The correct number was " + numberToGuess;
+            countGuesses();
+        }
+        else if(userinput < numberToGuess)
+        {
+            document.getElementById("demo").innerHTML = "Too low";
+            document.getElementById("demo2").innerHTML = "The correct number was " + numberToGuess;
+            countGuesses();
+        }
+        else 
+        {
+            document.getElementById("demo").innerHTML = "Well Done!!!";
+            document.getElementById("demo2").innerHTML = "The correct number was " + numberToGuess;
+            countGuesses();
+        }
+    //} 
+
+    //while(i < 6);
+    
 
 function countGuesses()
     {
